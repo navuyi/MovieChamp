e3a34e71449712888a768a975de864c539dda554
navuyi <figlusrafal@gmail.com>
2023-08-11 01:04:21 +0200

    app setup, updating packages

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,27 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { Provider } from 'react-redux';
-import { store } from './src/redux/store';
-import Counter from './src/components/Counter';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+import { Provider } from 'react-redux'
+import { store } from './src/redux/store'
+import { NavigationContainer } from '@react-navigation/native'
+import RootNavigator from './src/navigation/RootNavigator'
+import { useEffect } from 'react'
 
 export default function App() {
- 
+  useEffect(() => {
+    return () => {}
+  }, [])
+
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Text>Hello World</Text>
-        <Counter />
-        <StatusBar style="auto" />
-      </View>
+      <NavigationContainer>
+        <RootNavigator />
+      </NavigationContainer>
     </Provider>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
